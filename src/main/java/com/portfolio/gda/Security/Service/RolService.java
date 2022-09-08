de9822684d0f46590fb46e4cf8fc4211e049d750
Guillermo <guillermo.arena@gmail.com>
2022-09-08 14:21:19 -0300

    nuevo repositorio

---
+++ src/main/java/com/portfolio/gda/Security/Service/RolService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.gda.Security.Service;
+
+import com.portfolio.gda.Security.Entity.Rol;
+import com.portfolio.gda.Security.Enums.RolNombre;
+import com.portfolio.gda.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author guill
+ */
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
